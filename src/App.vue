6b67cad0f878eb25d8675e480ab7796e1d15f21e
Razi Mutussin <razi@lizard-apps.com>
2023-09-13 11:34:10 +0800

    feat(update): add AWS

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ const skills = ref([
   'Java', 'SpringBoot', 'Bash', 'Docker', 'Linux',
   'MS SQL Server', 'MySQL', 'Sqlite', 'Git', 'Electron',
   'Postgres', 'Firebase', 'ReactJS', 'React Native',
-  'NextJS', 'MongoDB', 'Scrum', 'Agile'
+  'NextJS', 'MongoDB', 'Azure', 'AWS'
 ])
 
 const showBorder = ref(false)
@@ -80,7 +80,7 @@ const showBorder = ref(false)
                 Improved and managed a collaborative document editor and a remote signing platform that leverage blockchain technology through <strong>Firebase</strong> and <strong>ReactJS</strong>. This platforms enable users to collaboratively create and edit contracts online in real-time, culminating in the secure signing and preservation of contracts on the blockchain.
               </li>
               <li>
-                Built and maintained mobile and web e-hailing application using <strong>ExpressJS</strong>, <strong>GraphQL</strong>, <strong>React Native</strong> and <strong>NextJS</strong>. This platform allow users to conveniently schedule taxi rides from any location in the Netherlands.
+                Developed, and consistently maintained e-hailing application for both mobile and web platforms using a tech stack comprising <strong>ExpressJS</strong>, <strong>GraphQL</strong>, <strong>React Native</strong>, and <strong>NextJS</strong>. Proficiently managed the continuous integration and continuous deployment (CI/CD) pipeline through <strong>Azure</strong>. This platform empowers users with the convenience to effortlessly schedule taxi rides from anywhere within the Netherlands.
               </li>
               <li>
                 Introduced <strong>Typescript</strong> to the project code base to have type safety within the large project. Improved project quality and reduced compile-time errors to 100%.
@@ -105,7 +105,7 @@ const showBorder = ref(false)
             <span class="leading-5 text-lg font-light">Wen Consulting Pte. Ltd. (Singapore)</span>
             <ul class="list-disc pl-4 text-sm text-justify">
               <li>
-                Built remote signing platform using <strong>ExpressJS</strong>, <strong>VueJS</strong> and <strong>Java</strong> that allow users to digitally sign pdf documents using SingPass application that comply with Singapore law, improving document verification rate during COVID-19 pandemic.
+                Developed a robust remote signing platform leveraging <strong>ExpressJS</strong>, <strong>VueJS</strong>, and <strong>Java</strong>, enabling users to perform <strong>digital signatures on PDF documents</strong> through the SingPass application, in full compliance with Singaporean legal requirements. This innovative solution significantly enhanced document verification rates, particularly during the challenging times of the COVID-19 pandemic. Leveraged <strong>AWS Serverless functions</strong> for efficient and scalable backend deployment.
               </li>
               <li>
                 Built and maintained a module in the company's main product that handles user commission calculation and commission report generation using <strong>NestJS</strong>, <strong>PuppeteerJS</strong> and <strong>VueJS</strong>.
@@ -119,6 +119,15 @@ const showBorder = ref(false)
             </ul>
           </div>
         </li>
+      </ol>
+    </div>
+  </div>
+  <div
+    class="page"
+    :class="{ 'show-page-border': showBorder }"
+  >
+    <div class="flex flex-col justify-start items-start px-4 pt-4">
+      <ol class="relative border-l border-gray-200 space-y-2">
         <li class="ml-4">
           <div class="absolute w-3 h-3 primary-bg rounded-full mt-1 -left-1.5 border border-white"></div>
           <div class="flex justify-between items-center mb-2 primary-text text-sm">
@@ -141,15 +150,6 @@ const showBorder = ref(false)
             </ul>
           </div>
         </li>
-      </ol>
-    </div>
-  </div>
-  <div
-    class="page"
-    :class="{ 'show-page-border': showBorder }"
-  >
-    <div class="flex flex-col justify-start items-start px-4 pt-4">
-      <ol class="relative border-l border-gray-200 space-y-2">
         <li class="ml-4">
           <div class="absolute w-3 h-3 primary-bg rounded-full mt-1 -left-1.5 border border-white"></div>
           <div class="flex justify-between items-center mb-2 primary-text text-sm">
@@ -168,25 +168,6 @@ const showBorder = ref(false)
               </li>
               <li>
                 Monitored real time drilling parameters and gas reading to ensure the safety of drilling operation.
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li class="ml-4">
-          <div class="absolute w-3 h-3 primary-bg rounded-full mt-1 -left-1.5 border border-white"></div>
-          <div class="flex justify-between items-center mb-2 primary-text text-sm">
-            <span class="leading-5">August, 2016 - October, 2016</span>
-            <div class="flex items-center">
-              <svg class="w-3 h-3 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-              <span>Kuala Lumpur, Malaysia</span>
-            </div>
-          </div>
-          <div class="flex flex-col">
-            <span class="leading-5 text-base font-bold">Internship</span>
-            <span class="leading-5 text-lg font-light">Kebabangan Petroleum Operating Company (Malaysia)</span>
-            <ul class="list-disc pl-4 text-sm text-justify">
-              <li>
-                Modeled oil reservoir's water saturation and permeability to help in finding out the reasons in huge differences between static and dynamic permeability reading acquired.
               </li>
             </ul>
           </div>
